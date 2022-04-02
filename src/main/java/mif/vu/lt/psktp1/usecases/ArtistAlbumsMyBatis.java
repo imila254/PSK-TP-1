d12efe5efbe321711038642ce855686dfc32aafe
imila <imila@DESKTOP-P03MSJS>
2022-04-02 15:23:57 +0300

    Mybatis implementation.

---
+++ src/main/java/mif/vu/lt/psktp1/usecases/ArtistAlbumsMyBatis.java
@@ -0,0 +1,54 @@
+package mif.vu.lt.psktp1.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import mif.vu.lt.psktp1.mybatis.model.Artist;
+import mif.vu.lt.psktp1.mybatis.model.Album;
+import mif.vu.lt.psktp1.mybatis.dao.AlbumMapper;
+import mif.vu.lt.psktp1.mybatis.dao.ArtistMapper;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+
+@Model
+public class ArtistAlbumsMyBatis {
+
+    @Inject
+    ArtistMapper artistMapper;
+
+    @Inject
+    AlbumMapper albumMapper;
+
+    @Getter @Setter
+    private Artist artist;
+
+    @Getter
+    private List<Album> albumList;
+
+    @Getter @Setter
+    private Album albumToCreate = new Album();
+
+    @PostConstruct
+    public void init(){
+        Map<String,String> requestParameters =
+                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+
+        Integer artistId = Integer.parseInt(requestParameters.get("artistId"));
+
+        this.artist = artistMapper.selectByPrimaryKey(artistId);
+
+    }
+
+    public void loadAllAlbums(){this.albumList = albumMapper.selectAll();}
+
+    @Transactional
+    public void createAlbum(){
+        albumToCreate.setAlbumArtist(this.artist.getArtistName());
+    }
+
+}
